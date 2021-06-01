d03417540763b2124ff9f9d87504810dd856f2b3
Tyler Rogers <tsrog04@gmail.com>
2021-06-01 11:34:24 -0500

    Final product - Website Tutorial - JS,HTML,CSS

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,31 @@
-const myHeading = document.queryselector('h1');
-myHeading.textContent = 'Hello World!';
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/website1.jpg') {
+        myImage.setAttribute('src','images/website2.jpg');
+    } else {
+        myImage.setAttribute('src','images/website1.jpg');
+    }
+}
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName) {
+        setUserName();
+    } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Welcome to my biography,' + myName;
+    }
+}
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textcontent = 'Welcome to my biography,' + storedName;
+}
+myButton.onclick = function() {
+    setUserName();
+}
